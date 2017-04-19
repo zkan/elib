8367ba3b458f5a3f157123a4c15d443b0ddea613
zkan <kan@prontomarketing.com>
2017-04-19 10:06:02 +0700

    Specify dependencies providing web app libraries

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject elib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "e-Library"
+  :url "https://www.kanouivirach.com/"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies
+    [[org.clojure/clojure "1.1.0"]
+     [org.clojure/clojure-contrib "1.1.0"]
+     [ring/ring-core "0.2.0"]
+     [ring/ring-jetty-adapter "0.2.0"]]
+  :dev-dependencies
+    [[ring/ring-devel "0.2.0"]])
