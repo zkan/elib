0a66caf0e9b763e68465773a38066e9f53694b01
zkan <kan@prontomarketing.com>
2017-04-20 11:20:43 +0700

    Upgrade package version

--- project.clj
+++ project.clj
@@ -4,9 +4,6 @@
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies
-    [[org.clojure/clojure "1.1.0"]
-     [org.clojure/clojure-contrib "1.1.0"]
-     [ring/ring-core "0.2.0"]
-     [ring/ring-jetty-adapter "0.2.0"]]
-  :dev-dependencies
-    [[ring/ring-devel "0.2.0"]])
+    [[org.clojure/clojure "1.8.0"]
+     [ring/ring-core "1.5.0"]
+     [ring/ring-jetty-adapter "1.5.0"]])
